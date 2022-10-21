780101d9834d883be7229b54ae05e7f1dc887700
CLedsonB <74743220+CLedsonB@users.noreply.github.com>
2022-10-21 10:17:15 -0300

    Add files via upload

---
+++ baixaAqui.bat
@@ -0,0 +1,33 @@
+echo off
+echo ----INICIANDO DOWNLOADS-----
+
+cd $$INSIRA SEU ENDEREÇO DE DOWNLOAD$$
+
+color a
+echo ´´
+echo -----------VIDEO 1-----------------
+youtube-dl -x --audio-format mp3 --audio-quality 0 $$INSIRA O LINK DO VIDEO$$
+
+color b
+echo ´´
+echo -----------VIDEO 2-----------------
+youtube-dl -x --audio-format mp3 --audio-quality 0 $$INSIRA O LINK DO VIDEO$$
+
+color c
+echo ´´
+echo -----------VIDEO 3-----------------
+youtube-dl -x --audio-format mp3 --audio-quality 0 $$INSIRA O LINK DO VIDEO$$
+
+color d
+echo ´´
+echo -----------VIDEO 4-----------------
+youtube-dl -x --audio-format mp3 --audio-quality 0 $$INSIRA O LINK DO VIDEO$$
+
+color e
+echo ´´
+echo -----------VIDEO 5-----------------
+youtube-dl -x --audio-format mp3 --audio-quality 0 $$INSIRA O LINK DO VIDEO$$
+
+echo -------------CONCLUIDO--------
+PAUSE
+
